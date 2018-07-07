8ba7b0f2e60cd59ad9402f2a3a378dc01f49de43
NightFullMoon <2406799112@qq.com>
2018-07-07 11:22:55 +0800

    添加了镜头移动的控制；添加了敌人移动的控制

---
+++ Assets/Script/Waypoints.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+    public static Transform[] positions;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void Awake()
+    {
+        positions = new Transform[transform.childCount];
+
+        for (int i = 0; i < positions.Length; ++i) {
+            positions[i] = transform.GetChild(i);
+        }
+    }
+}
---
+++ Assets/enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+
+    public float speed = 10;
+    private Transform[] positions;
+    private int index = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        positions = Waypoints.positions;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (positions.Length <= index)
+        {
+            return;
+        }
+
+        transform.Translate((positions[index].position - transform.position).normalized * Time.deltaTime * speed);
+
+        if (Vector3.Distance(positions[index].position, transform.position) < 0.2f)
+        {
+            ++index;
+        }
+    }
+}
